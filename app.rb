57b860b68f48e513d6807e04ca37fd4a26f37c1d
Zachary Schulz <zjschulz@gmail.com>
2019-12-20 01:07:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
 
+  get "/" do
+    erb :index
+  end
+  
+  get "/new" do
+    erb :create_puppy
+  end
+  
+  post "/puppy" do
+    @puppy = Puppy.new("1","2","3")
+    erb :display_puppy
+  end
+  
 end
